932c9d48f365038c6cb40710f3e1251aa12ec555
NamrataMeripo <namrata.meripo@gmail.com>
2019-08-24 15:18:51 -0400

    uploadsession

---
+++ mysecondsession.py
@@ -0,0 +1,20 @@
+
+
+#!/usr/bin//python
+
+msg = ("welcome to python world")
+
+name = ("my name is Nanrata Meripo")
+
+work = ("performance engineering")
+
+place = ("Heaven")
+
+print(msg)
+
+print(name)
+
+print(work)
+
+print(place)
+
